8eb7b00aa431a0da032d2c10644e1e8b9e4826dd
Ivan Shcherbyna <shcherbyna.ivan@gmail.com>
2017-07-06 22:32:51 +0300

    Add files via upload

---
+++ MultiCastChat/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace MultiCastChat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.lbPeople = new System.Windows.Forms.ListBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.tbChat = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.tbMessage = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.lbPeople);
+            this.splitContainer1.Panel1.Controls.Add(this.btnExit);
+            this.splitContainer1.Panel1.Controls.Add(this.btnEnter);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.tbName);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.tbChat);
+            this.splitContainer1.Panel2.Controls.Add(this.btnSend);
+            this.splitContainer1.Panel2.Controls.Add(this.tbMessage);
+            this.splitContainer1.Size = new System.Drawing.Size(565, 277);
+            this.splitContainer1.SplitterDistance = 188;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // lbPeople
+            // 
+            this.lbPeople.BackColor = System.Drawing.SystemColors.Menu;
+            this.lbPeople.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lbPeople.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.lbPeople.FormattingEnabled = true;
+            this.lbPeople.Location = new System.Drawing.Point(13, 145);
+            this.lbPeople.MultiColumn = true;
+            this.lbPeople.Name = "lbPeople";
+            this.lbPeople.ScrollAlwaysVisible = true;
+            this.lbPeople.Size = new System.Drawing.Size(153, 108);
+            this.lbPeople.TabIndex = 3;
+            this.lbPeople.MouseClick += new System.Windows.Forms.MouseEventHandler(this.lbPeople_MouseClick);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(12, 95);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(154, 23);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Выход";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(12, 65);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(154, 24);
+            this.btnEnter.TabIndex = 2;
+            this.btnEnter.Text = "Войти";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите имя";
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(12, 24);
+            this.tbName.Multiline = true;
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(154, 23);
+            this.tbName.TabIndex = 0;
+            // 
+            // tbChat
+            // 
+            this.tbChat.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tbChat.Location = new System.Drawing.Point(0, 0);
+            this.tbChat.Multiline = true;
+            this.tbChat.Name = "tbChat";
+            this.tbChat.Size = new System.Drawing.Size(298, 233);
+            this.tbChat.TabIndex = 0;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnSend.Location = new System.Drawing.Point(298, 0);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 233);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Отправить";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // tbMessage
+            // 
+            this.tbMessage.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tbMessage.Location = new System.Drawing.Point(0, 233);
+            this.tbMessage.Multiline = true;
+            this.tbMessage.Name = "tbMessage";
+            this.tbMessage.Size = new System.Drawing.Size(373, 44);
+            this.tbMessage.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(38, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "сейчас в чате: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 277);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "MultiCast CHAT";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.TextBox tbChat;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox tbMessage;
+        private System.Windows.Forms.ListBox lbPeople;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ MultiCastChat/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiCastChat
+{
+    public partial class Form1 : Form
+    {
+        bool alive = false; //означатет будет ли работать поток для приема
+        UdpClient client;
+        //для реально разных компьютеров необх. вынести порты в форму, они должны оличаться (отправка и получение)
+        const int LOCALPORT = 8001;// порт для приема сообщений
+        const int REMOTEPORT = 8001;// порт для отправки сообщений
+        const int TTL = 20; // time to leave
+        const string HOST = "235.5.5.1";//хост для групповой рассылки
+        string userName;//имя пользователя
+        IPAddress groupAddress;//адресс для групповой рассылки
+        
+       // List<String> peolpe = null;
+        public Form1()
+        {
+           
+            InitializeComponent();
+           
+            btnExit.Enabled = false;
+            btnSend.Enabled = false;
+            tbChat.ReadOnly = true;
+          //  peolpe = new List<string>();
+            groupAddress = IPAddress.Parse(HOST);
+            
+        }
+        
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            userName = tbName.Text;
+            tbName.ReadOnly = true;
+            try
+            {
+               
+                client = new UdpClient(LOCALPORT); // создаем клиента
+                //присоединяемся к групповой рассылке
+                client.JoinMulticastGroup(groupAddress, TTL);
+                Task.Factory.StartNew(RecieveMessages);
+
+                string message = userName + " вошел в чат";
+                lbPeople.Items.Add(userName);
+                lbPeople.Update();
+                byte[] data = Encoding.Default.GetBytes(message); //передаем всегда желательно байты, переводи их в байты чтобы знать их размер
+                client.Send(data, data.Length, HOST, REMOTEPORT);
+
+                btnEnter.Enabled = false;
+                btnExit.Enabled = true;
+                btnSend.Enabled = true;
+               
+            }
+            catch (Exception ex) { MessageBox.Show(ex.StackTrace.ToString());}
+        }
+
+        private void RecieveMessages() //метод приема сообщений
+        {
+            alive = true;
+            try
+            {
+                IPEndPoint remoteIp = null;
+                byte[] data = client.Receive(ref remoteIp);
+                string message = Encoding.Default.GetString(data);
+
+                //добавление сообщения в текстовое поле
+                Invoke(new Action(() =>
+                {
+                    string time = DateTime.Now.ToShortTimeString();
+                    tbChat.Text = time + " " + message + "\r\n" + tbChat.Text;
+                }));
+            }
+            catch(ObjectDisposedException ex)
+            {
+                if (!alive) { return; throw; }
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message);}
+        }
+
+        private void exit()
+        {
+            try
+            {
+                string message = userName + " покидает чат ";
+                byte[] data = Encoding.Default.GetBytes(message);
+                client.Send(data, data.Length, HOST, REMOTEPORT);
+                RecieveMessages();
+                client.DropMulticastGroup(groupAddress);//отсоединяемся от групповой рассылки
+                alive = false;
+                tbName.ReadOnly = false;
+                client.Close();
+
+                btnEnter.Enabled = true;
+                btnExit.Enabled = false;
+                btnSend.Enabled = false;
+                tbName.Enabled = true;
+            }
+            catch (Exception ex) { MessageBox.Show(ex.StackTrace.ToString());}
+        }
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            exit();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Task.Factory.StartNew(RecieveMessages);
+
+                string message;
+                message=userName+": "+ tbMessage.Text;
+                //string message = String;
+                byte[] data = Encoding.Default.GetBytes(message);
+                client.Send(data, data.Length, HOST, REMOTEPORT);
+                tbMessage.Clear();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.StackTrace.ToString());
+            }
+            
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //if(btnExit.Enabled==false) this.Close();
+            exit();
+            
+        }
+
+        private void lbPeople_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (lbPeople.Items != null)
+            {
+                tbMessage.Text += lbPeople.SelectedItem.ToString() + ", ";
+            }
+            else MessageBox.Show("Выбирите пожалуйта кому отправлять соощение хотите");
+
+
+        }
+    }
+}
